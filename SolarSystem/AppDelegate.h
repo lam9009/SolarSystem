0884681bbed7ef233cdd4d40b6d8a646e9d01a68
lam9009 <lam9009@yahoo.com>
2015-02-13 12:27:48 -0800

    Initial Commit

---
+++ SolarSystem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SolarSystem
+//
+//  Created by Alfred on 13/2/15.
+//  Copyright (c) 2015 Maptier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SolarSystem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SolarSystem
+//
+//  Created by Alfred on 13/2/15.
+//  Copyright (c) 2015 Maptier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
